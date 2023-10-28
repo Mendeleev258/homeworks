137039037da67ec781c6eb3a333034e5ce450951
Yaroslav Bolokhonov <jroslavl.2005@gmail.com>
2023-10-28 17:14:36 +0300

    Update lab2.cpp

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,25 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    
+
     short choice;
-    do 
+    do
     {
         std::cout << "1. Сумма заданного количества слагаемых\n";
         std::cout << "2. Сумма с заданной точностью\n";
         std::cout << "3. Завершение работы\n";
 
         bool isError = false;
-        
+
         do
         {
             isError = false;
             std::cout << ">>> ";
-            std::cin >> choice;
-            if (!(choice >= 1 && choice <= 3)) 
+            if (!(std::cin >> choice && choice >= 1 && choice <= 3))
             {
                 std::cout << "Ошибка ввода!\n";
                 std::cin.clear();
@@ -27,18 +26,17 @@ int main()
                 isError = true;
             }
         } while (isError);
-        
+
         if (choice != 3)
         {
             // input x
-            
+
             double x;
             do
             {
                 isError = true;
                 std::cout << "Введите (|x| < 1): ";
-                std::cin >> x;
-                if (abs(x) < 1)
+                if (std::cin >> x && abs(x) < 1)
                     isError = false;
                 else
                 {
@@ -60,10 +58,9 @@ int main()
                 for (double i = 0; i < n; ++i)
                 {
                     s += a;
-                    a *= (2 * i + 1) / (2 * i + 2) * x * (-1);
+                    a *= -x * (2 * i + 1) / (2 * i + 2);
                 }
 
-                std::cout << std::setw(26) << std::left << "Количество слагаемых" << " = " << n << '\n';
                 std::cout << std::setw(26) << std::left << "Точное значение" << " = " << std::fixed << std::setprecision(11) << 1.0 / sqrt(1 + x) << '\n';
                 std::cout << std::setw(26) << std::left << "Приближенное значение" << " = " << std::fixed << std::setprecision(11) << s << '\n';
             }
@@ -71,41 +68,34 @@ int main()
             {
                 // task 2
                 double s = 0, a = 1, eps, tau = 1.0 / sqrt(1 + x);
-                int n = 0;
+                int i = 0;
                 std::cout << "Введите eps = ";
                 std::cin >> eps;
                 std::cout << '\n';
 
-                for (double i = 0; abs(s - tau) > eps; ++i)
+                for (int j = 0; j < 2; ++j)
                 {
-                    n += 1;
-                    s += a;
-                    a *= (2 * i + 1) / (2 * i + 2) * x * (-1);
+                    while (abs(a) > eps)
+                    {
+                        s += a;
+                        a *= -x * (2. * i + 1) / (2. * i + 2);
+                        ++i;
+                    }
+                    std::cout << std::setw(26) << std::left << "Количество слагаемых" << " = " << i << '\n';
+                    std::cout << std::setw(26) << std::left << "Точное значение" << " = " << std::fixed << std::setprecision(11) << tau << '\n';
+                    std::cout << std::setw(26) << std::left << "Приближенное значение" << " = " << std::fixed << std::setprecision(11) << s << '\n' << '\n';
+                    eps /= 10;
                 }
-                
-                std::cout << std::setw(26) << std::left << "Количество слагаемых" << " = " << n << '\n';
-                std::cout << std::setw(26) << std::left << "Точное значение" << " = " << std::fixed << std::setprecision(11) << 1.0 / sqrt(1 + x) << '\n';
-                std::cout << std::setw(26) << std::left << "Приближенное значение" << " = " << std::fixed << std::setprecision(11) << s << '\n' << '\n';
-
-                for (double i = 0; abs(s - tau) > eps * 0.1; ++i)
-                {
-                    n += 1;
-                    s += a;
-                    a *= (2 * i + 1) / (2 * i + 2) * x * (-1);
-                }
-                std::cout << "Для eps на порядок меньше\n";
-                std::cout << std::setw(26) << std::left << "Количество слагаемых" << " = " << n << '\n';
-                std::cout << std::setw(26) << std::left << "Точное значение" << " = " << std::fixed << std::setprecision(11) << 1.0 / sqrt(1 + x) << '\n';
-                std::cout << std::setw(26) << std::left << "Приближенное значение" << " = " << std::fixed << std::setprecision(11) << s << '\n';
             }
-            
+
             std::cout << "\nЗавершить работу? (Y/любая клавиша): ";
-            
+
             char isExit;
             std::cin >> isExit;
-            
+
             if (isExit == 'Y' || isExit == 'y')
                 choice = 3;
         }
+
     } while (choice != 3);
 }
